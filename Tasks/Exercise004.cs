79ebd17de5ada61cc7b6ec7a4411439186c9a036
Kusum <kusumthapa_magar@hotmail.com>
2020-11-14 23:47:38 +0000

    Implemented task 4

--- Tasks/Exercise004.cs
+++ Tasks/Exercise004.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
 // Leave punctuation marks untouched.
@@ -9,7 +10,51 @@ namespace TechReturners.Tasks
     {
         public static string PigIt(string str)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                string append = "ay";
+
+                List<string> changedWords = new List<string>();
+
+                foreach (string s in str.Split(' '))
+                {
+                    if (s.Length == 1)
+                    {
+                        changedWords.Add(changedWords + append);
+                    }
+                    else
+                    {
+                        bool containsPunctuationMark = false;
+
+                        for (int i = 0; i < s.Length; i++)
+                        {
+                            if (char.IsPunctuation(s[i]))
+                            {
+                                containsPunctuationMark = true;
+                                changedWords.Add(s.Substring(1 ,  i - 1) + (string)s.Substring(0 , 1) + append + s.Substring(i));
+                                break;
+                            }
+                        }
+
+                        if (!containsPunctuationMark)
+                        {
+
+                            string appendedWord = s.Substring(1, s.Length - 1) + (string)s.Substring(0, 1) + append;                          
+
+                            changedWords.Add(appendedWord);
+                        }
+                       
+                    }
+
+                }
+                return string.Join(" ", changedWords);
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
